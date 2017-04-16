d77a583c0dbc4c253f0fb7bd7b7b3f820a453e6c
cerico <garethrobertlee@gmail.com>
2017-04-16 10:33:19 +0100

    init

---
+++ js/main.js
@@ -0,0 +1,45 @@
+document.querySelector('body').grains({
+  intensity: 1,
+  size: 625,
+  color: '#000000',
+  backgroundColor: '#fff',
+  opacity: 0.12,
+  monochrome: true
+})
+
+
+document.querySelector('.box1').grains({
+  intensity: 1,
+  size: 625,
+  color: '#ff0000',
+  backgroundColor: '#d3aa85',
+  opacity: 0.44,
+  monochrome: true
+})
+
+document.querySelector('.box2').grains({
+  intensity: 1,
+  size: 525,
+  color: '#000000',
+  backgroundColor: '#85b6d3',
+  opacity: 0.12,
+  monochrome: true
+})
+
+document.querySelector('.box3').grains({
+  intensity: 1,
+  size: 625,
+  color: '#ffffff',
+  backgroundColor: '#c292e0',
+  opacity: 0.72,
+  monochrome: true
+})
+
+document.querySelector('.box4').grains({
+  intensity: 1,
+  size: 625,
+  color: '#000000',
+  backgroundColor: '#aa76f3',
+  opacity: 0.14,
+  monochrome: true
+})
